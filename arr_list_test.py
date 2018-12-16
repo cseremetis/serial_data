77be4098cf03e2669aa9ae6a16251ad77e10b2e3
cseremetis <christianseremetis@gmail.com>
2018-12-16 23:00:47 +0000

    initial commit

---
+++ arr_list_test.py
@@ -0,0 +1,6 @@
+#list1=['hello','stop','move']
+list1=[]
+list1.append("hi")
+list1.append("nope")
+print list1[0]
+print list1[1]
--- read_serial_data.py
+++ read_serial_data.py
@@ -1,48 +1,69 @@
 import serial
+
+#OPEN SERIAL PORT
 ser = serial.Serial('/dev/ttyACM0')
 print(ser.name)
 #ser.write(b'hello')
+
+#OPEN log file
+logfile=open("logfile.txt",w)
+logfile.close()
+
 cnt=0;
-
 while True:
-    # using ser.readline() assumes each line contains a single reading
-    # sent using Serial.println() on the Arduino
+    #CASE 0: first time through loop
+    if cnt == 0:#need read until we get an ff
+		current=''#initialize current to empty
+		while current.encode('hex')!='ff':#keep reading until we get ff
+			current=ser.read(1)
+		cnt=2
+        
+	#CASE 1: second time current will have 'ff' from previous packet
+	
+	#MAKE THE LIST: read the whole packet into list_tmp
+	#	step 1: add the first ff to the list 
+    list_tmp=[]
+    list_tmp.append(current)
+	#	step 2: loop through until we hit the next ff adding to list
+    current='00'
+    while current.encode('hex')!='ff':
+        current=ser.read(1)
+        list_tmp.append(current)
 
-    #reading = ser.readline()#.decode('utf-8')
+    #COVERT LIST TO HEX: convert the list to hex and store it in converted list
+    list_converted=[]
+    #print len(list_tmp),
+    #print ": ",
+    for i in range(len(list_tmp)):
+        list_converted.append(list_tmp[i].encode('hex'))
+        #print the list out
+        #print list_converted[i],
+    #print " : ",
+    #print len(list_tmp),
+    #print
     
-    #reading = ser.read(1)#number is the number of bytes
-
-    #optionally could specify a specific number of bytes for specific decoding
-
-    # reading is a string...do whatever you want from here
-
-    #chop reading into substring and print char by char
-    #for i in range(len(reading)):
-        #print(reading(i))
-
-    #print (''.join(r'\x{02:x}'.format(ord(c)) for c in reading))
-
-
-    #print(int.from_bytes(reading))
-    #format(ord("c"), "x")
-
-
-    #if the first one read once
-    if cnt == 0:
-        reading=ser.read(1)#read one byte in initially
-        cnt=cnt+1
-    #read until we get an ff
-    while reading.encode('hex')!="ff":
-        reading=ser.read(1)
-
-
-    reading = ser.read(1)
+    ###########################################################################
+    ###########################################################################
+    #DECODE DATA:
+    #CASE 1: temperature data length = 18
+    #Case 2: read data length = 42,10
+    #CASE 3: read cycle keep alive
     
-    #print reading
-    tmp=reading.encode('hex')
-    print tmp
-    #print type(reading)
-    #print len(reading)
+    #CASE 1: temperature data
+    if len(list_converted)==18:
+        temp=list_converted[14]
+        temp2=int(temp, 16)
+        print "current reader temp Celcius: ",
+        print temp2
+        
+    #CASE 2: read data
+    if len(list_converted)==10:
+        print "tage read"
+        
+    
+    
+#CLOSE SERIAL PORT        
 ser.close()
+
 
 
