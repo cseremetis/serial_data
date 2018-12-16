8d3617bf35c302b06c111685d108e28c1a590636
senpai <christianseremetis@gmail.com>
2018-12-16 18:03:55 -0500

    minor changes

---
+++ decrypt.py
@@ -0,0 +1,19 @@
+
+import datetime
+
+max_data = 248 #maximum data size in bytes
+
+#for decrypting epc, tag crc, and message crc
+def setCRC(message, offset, length):
+    crc = 0xffff
+    
+    for i in range(offset, offset+length):
+        crc = ((crc << 4) | ((message[i] >> 4) & 0xf)) ^ crcTable[crc >> 12]
+        crc &= 0xffff
+        crc = ((crc << 4) | ((message[i] >> 0) & 0xf)) ^ crcTable[crc >> 12]
+        crc &= 0xffff
+
+    return crc
+
+def translate(hex):
+    #...
---
+++ temperature.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Dec 16 17:58:33 2018
+
+@author: chris
+"""
+
+import subprocess
+import time
+
