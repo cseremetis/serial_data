14ebe186670b475f0aef25eefa95d18bcf63e99b
cseremetis <christianseremetis@gmail.com>
2018-12-17 05:24:42 +0000

    updated

--- main.py
+++ main.py
@@ -2,29 +2,34 @@ import serial
 import datetime
 import time
 import subprocess
+import atexit #make sure device shuts down properly
 from temperature import cut_power, enable_power
 from read_serial_data import reset_log, scan
 
 enable_power() #allow power to flow to the reader
-time.sleep(3) #buffering time for power to return to port
+time.sleep(5) #buffering time for power to return to port
 
+reset_log()
+ser = serial.Serial('/dev/ttyACM0')
+print(ser.name)
+logfile=open("logfile.txt",'a')
+cnt=0
 while True:
-    reset_log()
-    ser = serial.Serial('/dev/ttyACM0')
-    print(ser.name)
-    logfile=open("logfile.txt",'a')
+    if cnt!=30:
+        scan(ser, logfile)
+        cnt=cnt+1
+    else:
+        ser.close()
+        cut_power()
+        time.sleep(3) #must call python -u to enable this
+        print("reloading reader")
+        enable_power()
+        time.sleep(3) #wait for power to return to the serial port
+        ser = serial.Serial('/dev/ttyACM0')
+        cnt=0
 
-    while True:
-        temp = scan(ser, logfile)
-        if (temp >= 40):
-            print("high temperature detected!")
-            ser.close()
-            cut_power()
-            time.sleep(25) #must call python -u to enable this
-            print("reloading reader")
-            enable_power()
-            time.sleep(3) #wait for power to return to the serial port
-            ser = serial.Serial('/dev/ttyACM0')
-
-cut_power() #shut down power to port on termination
-logfile.close()
+@atexit.register
+def goodbye():
+    ser.close() #shut down the com port
+    cut_power() #shut down power to port on termination
+    logfile.close()
