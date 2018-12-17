3f7bd026f8326443da09ae87791e1a813d035f04
senpai <christianseremetis@gmail.com>
2018-12-16 21:57:04 -0500

    allow for timeouts

--- main.py
+++ main.py
@@ -5,17 +5,22 @@ import subprocess
 import read_serial_data
 import temperature
 
-reset_log()
-ser = serial.Serial('/dev/ttyACM0')
-print(ser.name)
-temp
-logfile=open("logfile.txt",'a')
-
 while True:
-    temp = scan(ser, logfile)
-    if (temp >= 50):
-        cut_power()
-        time.sleep(15000) #probably have to call python -u to enable this
-        enable_power()
-
-logfile.close()
+    print "fire1"
+    time.sleep(1000)
+    print "fire2"
+# reset_log()
+# ser = serial.Serial('/dev/ttyACM0')
+# print(ser.name)
+# temp
+# logfile=open("logfile.txt",'a')
+#
+# while True:
+#     temp = scan(ser, logfile)
+#     if (temp >= 50):
+#         print "high temperature detected!",
+#         cut_power()
+#         time.sleep(15000) #probably have to call python -u to enable this
+#         enable_power()
+#
+# logfile.close()
